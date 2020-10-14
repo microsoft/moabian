74f6181276dc5ee18e3a3a7eae6e828228968c98
Scott Stanfield <scott.stanfield@microsoft.com>
2020-10-13 23:56:36 -0700

    Dropped old version of source

--- sw/stream/rpi/source.py
+++ sw/stream/rpi/source.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python3
-import sys
-import cv2
 import time
 import imagezmq
-import traceback
-import numpy as np
 from imutils.video import VideoStream
 
-jpg_quality = 90
+sender = imagezmq.ImageSender(connect_to='tcp://localhost:5555')
 
-try:
-    sender = imagezmq.ImageSender(connect_to='tcp://ss-mbp16:5555')
-    picam = VideoStream(usePiCamera=True, resolution=(512,512)).start()
-    time.sleep(0.5)  # allow camera sensor to warm up
-
-    while True:  # send images as stream until Ctrl-C
-        image = picam.read()
-        #ret_code, jpg_buffer = cv2.imencode(".jpg", image,
-        #       [int(cv2.IMWRITE_JPEG_QUALITY), jpg_quality])
-        #response = sender.send_jpg("source", jpg_buffer)
-        sender.send_image("moab", image)
-
-except (KeyboardInterrupt, SystemExit):
-    pass  # Ctrl-C was pressed to end program
-except Exception as ex:
-    print('Traceback error:', ex)
-    traceback.print_exc()
-finally:
-    picam.stop()
-    sys.exit()
+picam = VideoStream(usePiCamera=True).start()
+time.sleep(0.5)
+while True:
+    image = picam.read()
+    sender.send_image("moab", image)
