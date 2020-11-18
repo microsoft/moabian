8864cac4c577bb121ccc97038226caddb1dcd7ce
Scott Stanfield <scott.stanfield@microsoft.com>
2020-11-17 17:55:41 -0800

    Code reformat through black

--- bin/_offset.py
+++ bin/_offset.py
@@ -1,4 +1,4 @@
-#!foo.sh
+#!runpy.sh
 
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
@@ -73,8 +73,7 @@ def clamp(num, min_value, max_value):
 
 def parse_arguments():
     p = argparse.ArgumentParser(
-        prog="offset",
-        description="Determine servo power offsets"
+        prog="offset", description="Determine servo power offsets"
     )
 
     p.add_argument(dest="servo1", type=int, help="Integer offset [-5 ... 5]")
@@ -82,16 +81,29 @@ def parse_arguments():
     p.add_argument(dest="servo3", type=int, help="Integer offset [-5 ... 5]")
 
     p.add_argument("-i", "--ignore", required=False, action="store_true")
+    p.add_argument(
+        "-p",
+        "--print",
+        required=False,
+        action="store_true",
+        help="Show the current offset values and quit",
+    )
 
-    p.add_argument("-s", "--save",
-                   required=False,
-                   action="store_true",
-                   help="save settings to config.json")
+    p.add_argument(
+        "-s",
+        "--save",
+        required=False,
+        action="store_true",
+        help="save settings to config.json",
+    )
 
-    p.add_argument("-f", "--file",
-                   required=False,
-                   action="store",
-                   help="optional path to existing calibration.json file")
+    p.add_argument(
+        "-f",
+        "--file",
+        required=False,
+        action="store",
+        help="optional path to existing calibration.json file",
+    )
 
     args = p.parse_args()
     return args
@@ -105,17 +117,18 @@ def get_env(key, default):
 
 
 def get_file_ownership(filename):
-    return (
-        os.stat(filename).st_uid,
-        os.stat(filename).st_gid
-    )
+    return (os.stat(filename).st_uid, os.stat(filename).st_gid)
 
 
-def search_for_config():
-    # Heuristic to find config file
+def search_for_config(path=None):
+    assert path is None or type(path) == str
+
+    # If a --file flag was passed, use that location
+    if path:
+        return os.path.abspath(args.file)
 
     # 1. Rely on $MOABDIR if available
-    if os.environ.get("MOABDIR") is not None:
+    if os.environ.get("MOABDIR"):
         path = os.environ["MOABDIR"] + "/config/calibration.json"
         if os.path.isfile(path):
             return path
@@ -130,80 +143,47 @@ def search_for_config():
     if os.path.isfile(path):
         return path
 
-    return None
+    raise ValueError(f"{path} does not exist")
 
-def save_configuration(args):
-    if args.file is not None:
-        path = os.path.abspath(args.file)
-    else:
-        path = search_for_config()
 
-    if path is None:
-        print("Unable to find calibration.json file: use --file to specify", file=sys.stderr)
-        exit(-1)
+def print_offsets(args):
+    path = search_for_config(args.file)
 
     if os.path.isfile(path) is False:
-        print(f"{path} is not a file", file=sys.stderr)
-        exit(-1)
+        raise ValueError(f"{path} is not a file")
+
+    try:
+        print(f"path:{path}")
+        with open(path, "r") as f:
+            data = json.load(f)
+            json.dump(data, sys.stdout, indent=2)
+    except Exception as e:
+        print(f"Failed to read {path}")
+        print(e)
+
+
+def save_offsets(args):
+    path = search_for_config(args.file)
+
+    if os.path.isfile(path) is False:
+        raise ValueError(f"{path} is not a file")
 
     # These are the offsets to override
-    offsets = { "servoOffsets": [args.servo1, args.servo2, args.servo3] }
+    offsets = {"servoOffsets": [args.servo1, args.servo2, args.servo3]}
 
     # First read existing contents...
     try:
         print(f"path:{path}")
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             data = json.load(f)
 
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             data.update(offsets)
-            json.dump(data, f, indent = 2)
+            json.dump(data, f, indent=2)
 
     except Exception as e:
         print(f"Failed to write {offsets} to {path}")
         print(e)
-
-
-def tween(s1, s2, s3):
-    init()
-    sync()
-    sleep(0.1)
-
-    setTextIcon(Text.INIT, Icon.DOT)
-
-    #setServoOffsets(s1, s2, s3)
-    #setPlateAngles(0, 0)
-    sync()
-    sleep(0.1)
-    setTextIcon(Text.CAL_COMPLETE, Icon.DOT)
-
-    print("Tweening")
-    for n in range(155, 130, -1):
-        print(n)
-        setServoPositions(n,n,n)
-        sync()
-        sleep(0.005)
-
-    print("Press the menu button or CTRL-C to quit...")
-    sleep(0.1)
-    while not getMenuBtn():
-        sleep(0.01)
-        sync()
-
-    shutdown()
-
-def hover():
-    path = search_for_config()
-
-    print(f"path:{path}")
-    offsets = []
-
-    with open(path, 'r') as f:
-        data = json.load(f)
-        offsets = data['servoOffsets']
-
-    print(offsets)
-    tween(*offsets)
 
 
 if __name__ == "__main__":
@@ -211,12 +191,11 @@ if __name__ == "__main__":
 
     signal(SIGINT, sigint)
 
-    if args.ignore:
-        hover()
-    elif args.save:
-        save_configuration(args)
+    if args.save:
+        save_offsets(args)
+    elif args.print:
+        print_offsets(args)
     else:
         set_servo_offsets(args.servo1, args.servo2, args.servo3)
 
     exit(0)
-
