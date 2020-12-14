f2bbaeca0caa9f516f7bbb04b8a2115b817152ed
Scott Stanfield <scott.stanfield@microsoft.com>
2020-12-13 23:36:12 -0800

    Live stream the camera to http://moab.local:5000
    For this commit, only bare-metal dev works:
    
    (In window 1)
    
    ```
    cd ~/moab/sw
    sudo python3 main.py -t 1 --device=manual
    ```
    
    (In window 2)
    
    ```
    cd ~/moab/sw/stream
    pip3 install -r requirements.txt
    ```
    
    Then open your browser to http://moab.local:5000

--- sw/control/debug.py
+++ sw/control/debug.py
@@ -1,9 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import cv2
-
 from .common import IDebugDecorator
+import cv2
+import os
+import pathlib
+import logging as log
 
 
 SENSOR_IMG_ARG = "sensor_img"
@@ -32,7 +34,35 @@ class X11Decorator(CallbackDecorator):
         super().decorate(args)
 
         cv2.imshow(self.config["windowName"], args[SENSOR_IMG_ARG])
-        cv2.waitKey(1) & 0xFF
+        cv2.waitKey(1) & 0xff
 
     def __del__(self):
         cv2.destroyAllWindows()
+
+
+class FileDecorator(CallbackDecorator):
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+        self.filename = self.config["filename"]
+        self.disable = False
+
+        # Create path to filename in case it doesn't exist
+        dirname = os.path.dirname(self.filename)
+        pathlib.Path(dirname).mkdir(parents=True, exist_ok=True)
+
+        log.info(f"Saving camera frame to {self.filename}")
+
+    def decorate(self, args):
+        super().decorate(args)
+
+        if self.disable:
+            return
+
+        try:
+            image = args[SENSOR_IMG_ARG]
+            cv2.imwrite(self.filename, image, [cv2.IMWRITE_JPEG_QUALITY, 70])
+
+        except Exception as ex:
+            self.disable = True
+            log.error(ex)
