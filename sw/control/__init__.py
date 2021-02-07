3eb5c0aeb4c4506389d729a0a66665f5c4c317e3
Scott Stanfield <scott.stanfield@microsoft.com>
2021-02-07 13:31:40 -0800

    First tranche of v2.x code remove

--- sw/control/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-from .debug import *
-from .device import *
-from .timers import *
-from .perfcounters import *
--- sw/control/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-from .pid_controller import PIDController
-from .menu_controller import MenuController
-from .info_controller import InfoController
-from .brain_controller import BrainController
-from .huecal_controller import HueCalibrationController
-from .joystick_controller import JoystickController
