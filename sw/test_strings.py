734123f12db64491442e718fea442b5f009a967a
Scott Stanfield <scott.stanfield@microsoft.com>
2021-02-28 19:45:56 -0800

    is it actually working

---
+++ sw/test_strings.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import time
+from hat import Hat, Icon, Text, plate_angles_to_servo_positions
+
+menus = {
+    0: "Sacramento\nVacaville\nBerkeley CA",
+    1: "Sacramento\nVacaville\nBerkeley\nSan Luis Obispo CA",
+
+}
+
+def main(menu=0, frequency=10, debug=True):
+    idx = menu
+
+    with Hat() as hat:
+        hat.print_arbitrary_string(menus[idx])
+
+        while True:
+            hat.noop()
+            menu_btn, joy_btn, joy_x, joy_y = hat.poll_buttons()
+            time.sleep(1 / frequency)
+
+            # if menu_btn:
+            #     break;
+
+            # if joy_btn:
+            #     break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--menu",
+        default="0",
+        type=int,
+        choices=list(menus.keys()),
+        help=f"""Select what type of action to take.
+        """,
+    )
+    args, _ = parser.parse_known_args()
+    main(args.menu)
