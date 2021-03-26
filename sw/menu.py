5e6f6a9438e2a2e27c7ff6a66e2a3deef707bb14
Scott Stanfield <scott.stanfield@microsoft.com>
2021-03-26 10:09:18 -0700

    SW 3.0.24

--- sw/menu.py
+++ sw/menu.py
@@ -164,7 +164,7 @@ def _handle_debug(ctx, param, debug):
 
 
 @click.command()
-@click.version_option(version="3.0.23")
+@click.version_option(version="3.0.24")
 @click.option(
     "-c",
     "--cont",
