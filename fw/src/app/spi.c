c93ee4b5cbd255321a9eeb19a76d98c75a5a4c34
Scott Stanfield <scott@stanfield.io>
2021-03-30 11:45:30 -0700

    v3.0.28

--- fw/src/app/spi.c
+++ fw/src/app/spi.c
@@ -36,7 +36,6 @@ void spi_task(void)
 {
 	struct device *spi;
 	struct spi_config       spi_cfg;
-	struct spi_cs_control   spi_cs;
 
 	spi = device_get_binding("SPI_1");
 	if (!spi)
@@ -47,15 +46,9 @@ void spi_task(void)
 
 	spi_cfg.operation = SPI_WORD_SET(8) | SPI_OP_MODE_SLAVE;
 	spi_cfg.frequency = 25000000;
+	spi_cfg.cs = NULL;
 
-    // Past versions:
-    // spi_cs.gpio_dev = device_get_binding(DT_INST_0_ST_STM32_SPI_CS_GPIOS_CONTROLLER);
-	spi_cs.gpio_dev = NULL;
-	spi_cs.gpio_pin = DT_INST_0_ST_STM32_SPI_CS_GPIOS_PIN;
-	spi_cs.delay = 20;
-	spi_cfg.cs = &spi_cs;
-
-	LOG_INF("SPI interface CS in peripheral mode");
+	LOG_INF("SPI interface CS hardware mode");
 
 	const struct spi_buf tx_buf = {
 		.buf = &hat_to_pi,
@@ -71,9 +64,6 @@ void spi_task(void)
 
 	while(true)
 	{
-        // Peter's fix:
-        gpio_pin_configure(spi_cs.gpio_dev, DT_INST_0_ST_STM32_SPI_CS_GPIOS_PIN, GPIO_DIR_IN);
-
         memset(&pi_to_hat, 0, sizeof(pi_to_hat));       // 8 bytes from Raspberry Pi (Tx)
         memset(&hat_to_pi, 0, sizeof(hat_to_pi));       // 8 bytes to   Raspberry Pi (Rx)
 
--- sw/menu.py
+++ sw/menu.py
@@ -164,7 +164,7 @@ def _handle_debug(ctx, param, debug):
 
 
 @click.command()
-@click.version_option(version="3.0.25")
+@click.version_option(version="3.0.28")
 @click.option(
     "-c",
     "--cont",
