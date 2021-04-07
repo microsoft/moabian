1779acb932a35026571743f725d268fa8fee2d91
Peter Cornwell <peter.cornwell@freshconsulting.com>
2021-04-06 17:31:27 -0700

    Added comments and cleaned-up log messages

--- fw/src/app/spi.c
+++ fw/src/app/spi.c
@@ -20,6 +20,9 @@ fifo_item_t fifo_rx_queue;
 
 K_FIFO_DEFINE(my_fifo);
 
+// Read message received from the pi out of the FIFO
+// Call not thread safe. Run from one thread at a time.
+// Returned message buffer must be freed after processing is done.
 int wait_for_pi_message(pi_to_hat_t* msg, int32_t timeout)
 {
     fifo_item_t *rx_data = k_fifo_get(&my_fifo, timeout);
@@ -32,14 +35,16 @@ int wait_for_pi_message(pi_to_hat_t* msg, int32_t timeout)
 	return 0;
 }
 
+// Zephyr thread to read and write data over the
+// SPI bus to/from the pi.
 void spi_task(void)
 {
 	struct device *spi;
 	struct spi_config       spi_cfg;
 	struct spi_cs_control   spi_cs;
 	int spi_trancieve_ret = 0;
-	//int cnt = 0;
 
+	// Get SPI device binding from Zephyr device tree
 	spi = device_get_binding("SPI_1");
 	if (!spi)
 	{
@@ -47,6 +52,10 @@ void spi_task(void)
 		return;
 	}
 
+	// Set-up config struct for SPI
+	// Note: using custom patch for Zephyr 2.1 SPI driver to correct operation
+	// in slave mode. OS upgrades will need the patch implemented in spi_ll_stm32.c
+	// and spi_context.h. spi_cfg.cs must be set for patch to work correctly.
 	spi_cfg.operation = SPI_WORD_SET(8) | SPI_OP_MODE_SLAVE;
 	spi_cfg.frequency = 25000000;
 
@@ -57,6 +66,7 @@ void spi_task(void)
 
 	LOG_INF("SPI interface listening");
 
+	// Init Rx and Tx buffers
 	const struct spi_buf tx_buf = {
 		.buf = &hat_to_pi,
 		.len = sizeof(hat_to_pi)
@@ -69,8 +79,10 @@ void spi_task(void)
 	const struct spi_buf_set tx = { .buffers = &tx_buf, .count = 1 };
 	const struct spi_buf_set rx = { .buffers = &rx_buf, .count = 1 };
 
+	// Infinite loop in dedicated thread to transfer data over SPI bus
 	while(true)
 	{
+		// Fill Tx buff with message for PI
 		memset(&pi_to_hat, 0, sizeof(pi_to_hat));       // 8 bytes from Raspberry Pi (Tx)
         memset(&hat_to_pi, 0, sizeof(hat_to_pi));       // 8 bytes to   Raspberry Pi (Rx)
 
@@ -82,16 +94,23 @@ void spi_task(void)
         // tx: outbound (button state and joystick positions)
         // rx: incoming (command verbs)
 
+		// Send and recieve data from the Pi. No limit on wait length.
 		spi_trancieve_ret = spi_transceive(spi, &spi_cfg, &tx, &rx);
+
+		// Process transfer results
 		if(spi_trancieve_ret != sizeof(pi_to_hat))
 		{
-			if(spi_trancieve_ret >= 0)
-				LOG_ERR("Recieved %d bytes from pi.", spi_trancieve_ret);
-			else
-				LOG_ERR("SPI1 Error: %d", spi_trancieve_ret);
+			// did not recieve full length packet or an error was encountered
+			// in the SPI driver during the transfer
+
+			if(spi_trancieve_ret >= 0) // was the data the wrong length?
+				LOG_INF("Recieved %d bytes from pi but expected %d.", spi_trancieve_ret, sizeof(pi_to_hat));
+			else // Internal SPI driver error
+				LOG_ERR("Pi-Hat spi_transceive() returned with error: %d", spi_trancieve_ret);
 		}
 		else
 		{
+			// Valid packet was recieved. Put on FIFO queue for later processing.
             size_t size = sizeof(fifo_item_t);
             fifo_item_t *ptr = (fifo_item_t *) k_malloc(size);
             __ASSERT_NO_MSG(ptr != 0);
