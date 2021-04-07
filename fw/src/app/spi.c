8f5b50cda2f2b773eee10331cf403c040b392137
Scott Stanfield (MSFT) <58437613+scotstan@users.noreply.github.com>
2021-04-07 12:40:23 -0700

    v3.0.30 (#54)
    Fixes intermittent firmware crash due to SPI controller/peripheral
    problem. Specifically SPI chip select (CS) isn't working in Zephyr 2.1
    for devices set to peripheral mode, only controller mode.
    
    So both Raspberry Pi and the STM32 Moab Hat were both asserting the CS
    bus, causing an intermittent and difficult bug to track down.
    
    Co-authored-by: Peter Cornwell <peter.cornwell@freshconsulting.com>

--- fw/src/app/spi.c
+++ fw/src/app/spi.c
@@ -20,6 +20,9 @@ fifo_item_t fifo_rx_queue;
 
 K_FIFO_DEFINE(my_fifo);
 
+// Read message received from the pi out of the FIFO
+// Call not thread safe. Run from one thread at a time.
+// Returned message buffer must be freed after processing is done.
 int wait_for_pi_message(pi_to_hat_t* msg, int32_t timeout)
 {
     fifo_item_t *rx_data = k_fifo_get(&my_fifo, timeout);
@@ -32,11 +35,15 @@ int wait_for_pi_message(pi_to_hat_t* msg, int32_t timeout)
 	return 0;
 }
 
+// Zephyr thread to read and write data over the
+// SPI bus to/from the pi.
 void spi_task(void)
 {
 	struct device *spi;
 	struct spi_config       spi_cfg;
+	int spi_ret = 0;
 
+	// Get SPI device binding from Zephyr device tree
 	spi = device_get_binding("SPI_1");
 	if (!spi)
 	{
@@ -44,12 +51,17 @@ void spi_task(void)
 		return;
 	}
 
+	// Set-up config struct for SPI
+	// Note: using custom patch for Zephyr 2.1 SPI driver to correct operation
+	// in slave mode. OS upgrades will need the patch implemented in spi_ll_stm32.c
+	// and spi_context.h. spi_cfg.cs must be set for patch to work correctly.
 	spi_cfg.operation = SPI_WORD_SET(8) | SPI_OP_MODE_SLAVE;
 	spi_cfg.frequency = 25000000;
 	spi_cfg.cs = NULL;
 
 	LOG_INF("SPI interface CS hardware mode");
 
+	// Init Rx and Tx buffers
 	const struct spi_buf tx_buf = {
 		.buf = &hat_to_pi,
 		.len = sizeof(hat_to_pi)
@@ -62,9 +74,11 @@ void spi_task(void)
 	const struct spi_buf_set tx = { .buffers = &tx_buf, .count = 1 };
 	const struct spi_buf_set rx = { .buffers = &rx_buf, .count = 1 };
 
+	// Infinite loop in dedicated thread to transfer data over SPI bus
 	while(true)
 	{
-        memset(&pi_to_hat, 0, sizeof(pi_to_hat));       // 8 bytes from Raspberry Pi (Tx)
+		// Fill Tx buff with message for PI
+		memset(&pi_to_hat, 0, sizeof(pi_to_hat));       // 8 bytes from Raspberry Pi (Tx)
         memset(&hat_to_pi, 0, sizeof(hat_to_pi));       // 8 bytes to   Raspberry Pi (Rx)
 
         hat_to_pi.menu_button = atomic_get(&g_btn_menu);
@@ -75,8 +89,25 @@ void spi_task(void)
         // tx: outbound (button state and joystick positions)
         // rx: incoming (command verbs)
 
-		if (spi_transceive(spi, &spi_cfg, &tx, &rx) > 0)
+		// Send and recieve data from the Pi. No limit on wait length.
+        // spi_ret >= 0 is number of bytes received, else
+        // spi_ret will be a negative errno code
+		spi_ret = spi_transceive(spi, &spi_cfg, &tx, &rx);
+
+		// Process transfer results
+		if(spi_ret != sizeof(pi_to_hat))
 		{
+			// did not recieve full length packet or an error was encountered
+			// in the SPI driver during the transfer
+
+			if(spi_ret >= 0) // was the data the wrong length?
+				LOG_INF("Got %d bytes from pi but expected %d.", spi_ret, sizeof(pi_to_hat));
+			else // Internal SPI driver error
+				LOG_ERR("spi_transceive() error: %d", spi_ret);
+		}
+		else
+		{
+			// Valid packet was received. Put on FIFO queue for later processing.
             size_t size = sizeof(fifo_item_t);
             fifo_item_t *ptr = (fifo_item_t *) k_malloc(size);
             __ASSERT_NO_MSG(ptr != 0);
