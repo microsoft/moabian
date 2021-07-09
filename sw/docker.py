cfea6729e8f49649b8e7b69765b0a2597b2ca025
JRAlexander <john.alexander@microsoft.com>
2021-07-09 16:36:40 -0700

    pytest docker.py test created

--- sw/docker.py
+++ sw/docker.py
@@ -3,50 +3,6 @@ import json
 import sys
 from dataclasses import dataclass
 
-# Image name Test cases 
-
-#    = image name more than 1 slash 
-
-#       = 1 colon, more than 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2-linux-arm32v7
-#       = 1 colon, no dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2
-#       = 1 colon, 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2-arm32v7
-
-#       = more than 1 colon, no dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2:er
-#       = more than 1 colon, 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2:linux-arm32v7
-#       = more than 1 colon, more than 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2:linux-arm32-v7
-
-#       = no colon, no dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle
-#       = no colon 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle-linux
-#       = no colon more than 1 dash  - scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle-linux-arm32v7
-
-#    = image name 1 slash 
-
-#       = no colon, no dash - moab/brain
-#       = no colon, 1 dash - moab/brain:3:3-linux
-#       = no colon, more than 1 dash - moab/brain:3:3-linux-v32
-
-#       = 1 colon, no dash - moab/brain:3
-#       = 1 colon, 1 dash - moab/brain:3-linux
-#       = 1 colon more than 1 dash - moab/brain:3-linux-v32
-
-#       = more than 1 colon, no dash - moab/brain:3:3
-#       = more than 1 colon, 1 dash - moab/brain:3:3-linux
-#       = more than 1 colon, more than 1 dash - moab/brain:3:3-linux-v32
-
-#     = image name no slash 
-
-#       = no colon no dash - den1
-#       = no colon 1 dash - 3-linux
-#       = no colon more than 1 dash - v2-02-rg
-
-#       = 1 colon no dash - den1:v2
-#       = 1 colon 1 dash - den1:3-linux
-#       = 1 colon more than 1 dash - den1:v2-02-rg
-
-#       = more than 1 colon no dash - den1:v2:hi
-#       = more than 1 colon more than 1 dash - den1:v2:-02-rg
-#       = more than 1 colon no dash - den1:v2:23
-
 @dataclass
 class BonsaiImage:
     image: str  # image     scotstanws.azurecr.io/d83f2142-1c3c-4ae4-84fa-4e9ff3aa5ed9/circle:2-linux-arm32v7
@@ -88,6 +44,55 @@ def reformat_json(stdout):
     json += "]"
     return json
 
+def get_port(splitports):
+    # port format: 'Ports': '0.0.0.0:5005->5000/tcp, :::5005->5000/tcp'
+    print(splitports)
+    if splitports is not None:
+        # split on comma first - 0.0.0.0:5005->5000/tcp
+        splitport_1 = splitports.split(",")[0]
+        # split next on arrow - 0.0.0.0:5005
+        splitport_2 = splitport_1.split("->")[0]
+        # finally split on colon and take last element - 5005
+        port = splitport_2.split(":")[1]
+    else:
+        port = 0
+    
+    return port
+
+def get_image_info(image_name, port):
+    # split image on slashes
+    version = 0
+    slashes = image_name.split("/")
+
+    # if image tag or no slashes, use the image name
+    if slashes is not None and len(slashes) == 1:
+        brain_id = image_name
+        short_name = brain_id[:9]
+    else:
+        # if there's a colon in the name, use it for version
+        colon = slashes[-1].split(":")
+        if colon and len(colon) > 1:
+            version_split = colon[1].split("-")
+            if version_split is not None:
+                version = version_split[0]
+                brain_id = colon[0]
+                short_name = brain_id[:6] + ":" + version
+            else:
+                brain_id = colon[0]
+                short_name = brain_id[:9]
+        else:
+            brain_id = image_name
+            short_name = brain_id[:9]
+
+    bonsai_image = BonsaiImage(
+        image=image_name,
+        brain_id=brain_id,
+        version=version,
+        short_name=short_name,
+        port=int(port),
+    )
+    return bonsai_image
+
 
 def list_to_bonsai_images(iot_dict):
     # list of BonsaiImages to return
@@ -95,55 +100,16 @@ def list_to_bonsai_images(iot_dict):
 
     # parse the iot_dict list
     for x, info in enumerate(iot_dict):
-
+             
         if (info["Names"] != "edgeHub") and (info["Names"] != "edgeAgent"):
             # check for port
-            if "Ports" in info.keys():
-                # port format: 'Ports': '0.0.0.0:5005->5000/tcp, :::5005->5000/tcp'
-                splitports = info["Ports"]
-
-                if splitports is not None:
-                    # split on comma first - 0.0.0.0:5005->5000/tcp
-                    splitport_1 = splitports.split(",")[0]
-                    # split next on arrow - 0.0.0.0:5005
-                    splitport_2 = splitport_1.split("->")[0]
-                    # finally split on colon and take last element - 5005
-                    port = splitport_2.split(":")[1]
-            version = 0
-            # split image on slashes
-            slashes = info["Image"].split("/")
-
-            # if image tag or no slashes, use the image name
-            if slashes is not None and len(slashes) == 1:
-                brain_id = info["Image"]
-                short_name = brain_id[:9]
-            else:
-                # if there's a colon in the name, use it for version 
-                colon = slashes[-1].split(":")
-                if colon and len(colon) > 1:
-                    version_split = colon[1].split("-")
-                    if version_split is not None:
-                        version = version_split[0]
-                        brain_id = colon[0]
-                        short_name = brain_id[:6] + ":" + version
-                    else:
-                        brain_id = colon[0]
-                        short_name = brain_id[:9]
-                else:
-                    brain_id = info["Image"]
-                    short_name = brain_id[:9]
-
-
-            bonsai_image = BonsaiImage(
-                image=info["Image"],
-                brain_id=brain_id,
-                version=version,
-                short_name=short_name,
-                port=int(port),
-            )
-            bonsai_images.append(bonsai_image)
+            if "Ports" in info.keys():                
+                port = get_port(info["Ports"])        
+                bonsai_image = get_image_info(info["Image"], port)
+                bonsai_images.append(bonsai_image)
+    
     return bonsai_images
 
-
 if __name__ == "__main__":
-    print(ps()[0])
+   # print(ps()[0])
+    print(ps())
