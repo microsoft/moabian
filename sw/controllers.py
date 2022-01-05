c9366a2c58c52a9c73f6681e5e4efb5f6c44aa44
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2022-01-04 18:49:53 -0800

    Use v1 endpoints even on v2 brains but safely

--- sw/controllers.py
+++ sw/controllers.py
@@ -54,110 +54,79 @@ def joystick_controller(max_angle=16, **kwargs):
     return next_action
 
 
-class BrainController:
+def brain_controller(
+    max_angle=22,
+    port=5555,
+    client_id=123,
+    alert_fn=lambda toggle: None,
+    **kwargs,
+):
     """
     This class interfaces with an HTTP server running locally.
     It passes the current hardware state and gets new plate
     angles in return.
-
     The hardware state is unprojected from camera pixel space
     back to real space by using the calculated plate surface plane.
+
+    Note: Still use v1 endpoint even if it's a v2 brain because on every new
+    creation of a brain controller we still call DELETE on the v2 brain
+    endpoint. This way we don't need to know information about what the trained
+    brain was called to navigate the json response.
     """
+    prediction_url = f"http://localhost:{port}/v1/prediction"
 
-    def __init__(
-        self,
-        max_angle=22,
-        port=5555,
-        client_id=12345,
-        alert_fn=lambda toggle: None,
-        **kwargs,
-    ):
-        # Test that port has a valid brain
-        status = requests.get(f"http://localhost:{port}/v1/status").status_code
-        valid_brain = status == 200
+    # Test that port has a valid brain
+    status = requests.get(prediction_url).status_code
+    if status != 200:
+        raise ValueError(f"Port {port} is not a valid Bonsai brain")
 
-        if not valid_brain:
-            raise ValueError(f"Port {port} is not a valid Bonsai brain")
+    # Test whether the brain is v1 or v2 (and reset memory if a v2 brain)
+    status = requests.delete(f"http://localhost:{port}/v2/clients/{client_id}")
+    version = 2 if status.status_code == 204 else 1
 
-        # Test whether the brain is v1 or v2 (also resets memory if a v2 brain)
-        status = requests.delete(
-            f"http://localhost:{port}/v2/clients/{client_id}"
-        ).status_code
-        self.version = 2 if status == 204 else 1
-
-        if self.version == 1:
-            self.prediction_url = f"http://localhost:{port}/v1/prediction"
-        elif self.version == 2:
-            self.prediction_url = (
-                f"http://localhost:{port}/v2/clients/{client_id}/predict"
-            )
-        else:
-            raise ValueError("Brain version `{self.version}` is not supported.")
-
-        self.client_id = client_id
-        self.max_angle = max_angle
-        self.alert_fn = alert_fn
-        self.port = port
-
-    def __call__(self, state):
+    def next_action(state):
         env_state, ball_detected, buttons = state
         x, y, vel_x, vel_y, sum_x, sum_y = env_state
 
         observables = {
-            "state": {"ball_x": x, "ball_y": y, "ball_vel_x": vel_x, "ball_vel_y": vel_y}
+            "ball_x": x,
+            "ball_y": y,
+            "ball_vel_x": vel_x,
+            "ball_vel_y": vel_y,
         }
 
         action = Vector2(0, 0)  # Action is 0,0 if not detected or brain didn't work
         info = {"status": 400, "resp": ""}
-
         if ball_detected:
-            # Trap on POST failures so we can restart the brain without
+
+            # Trap on GET failures so we can restart the brain without
             # bringing down this run loop. Plate will default to level
             # when it loses the connection.
             try:
                 # Get action from brain
-                response = requests.post(self.prediction_url, json=observables)
+                response = requests.post(prediction_url, json=observables)
                 info = {"status": response.status_code, "resp": response.json()}
-                action_json = response.json()
 
                 if response.ok:
-                    if self.alert_fn is not None:
-                        self.alert_fn(False)
-
-                    print(action_json)
-                    if self.version == 1:
-                        pitch = action_json["input_pitch"]
-                        roll = action_json["input_roll"]
-                    elif self.version == 2:
-                        pitch = action_json["concepts"]["MoveToCenter"]["action"][
-                            "input_pitch"
-                        ]
-                        roll = action_json["concepts"]["MoveToCenter"]["action"][
-                            "input_roll"
-                        ]
-                    else:
-                        raise ValueError(
-                            f"Brain version `{self.version}` is not supported."
-                        )
+                    if alert_fn is not None:
+                        alert_fn(False)
+                    pitch = info["resp"]["input_pitch"]
+                    roll = info["resp"]["input_roll"]
 
                     # Scale and clip
-                    pitch = np.clip(
-                        pitch * self.max_angle, -self.max_angle, self.max_angle
-                    )
-                    roll = np.clip(
-                        roll * self.max_angle, -self.max_angle, self.max_angle
-                    )
+                    pitch = np.clip(pitch * max_angle, -max_angle, max_angle)
+                    roll = np.clip(roll * max_angle, -max_angle, max_angle)
 
                     # To match how the old brain works (only integer plate angles)
                     pitch, roll = int(pitch), int(roll)
 
                     action = Vector2(-roll, pitch)
                 else:
-                    if self.alert_fn is not None:
-                        self.alert_fn(True)
+                    if alert_fn is not None:
+                        alert_fn(True)
 
             except requests.exceptions.ConnectionError as e:
-                print(f"No brain listening on port: {self.port}", file=sys.stderr)
+                print(f"No brain listening on port: {port}", file=sys.stderr)
                 raise BrainNotFound
 
             except Exception as e:
@@ -165,3 +134,4 @@ class BrainController:
 
         return action, info
 
+    return next_action
