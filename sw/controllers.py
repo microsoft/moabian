b19ba93d7428be22abaefd40012d313b1ee68e3d
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2022-01-06 10:58:41 -0800

    Use the v2 api but always use first concept...

--- sw/controllers.py
+++ sw/controllers.py
@@ -57,7 +57,7 @@ def brain_controller(
     max_angle=22,
     port=5555,
     client_id=123,
-    alert_fn=lambda toggle: None,
+    alert_fn=None,
     **kwargs,
 ):
     """
@@ -72,13 +72,18 @@ def brain_controller(
     endpoint. This way we don't need to know information about what the trained
     brain was called to navigate the json response.
     """
-    prediction_url = f"http://localhost:{port}/v1/prediction"
-
     # Reset memory if a v2 brain
     status = requests.delete(f"http://localhost:{port}/v2/clients/{client_id}")
     version = 2 if status.status_code == 204 else 1
 
-    def next_action(state):
+    if version == 1:
+        prediction_url = f"http://localhost:{port}/v1/prediction"
+    elif version == 2:
+        prediction_url = f"http://localhost:{port}/v2/clients/{client_id}/predict"
+    else:
+        raise ValueError("Brain version `{self.version}` is not supported.")
+
+    def next_action_v1(state):
         env_state, ball_detected, buttons = state
         x, y, vel_x, vel_y, sum_x, sum_y = env_state
 
@@ -102,8 +107,6 @@ def brain_controller(
                 info = {"status": response.status_code, "resp": response.json()}
 
                 if response.ok:
-                    if alert_fn is not None:
-                        alert_fn(False)
                     pitch = info["resp"]["input_pitch"]
                     roll = info["resp"]["input_roll"]
 
@@ -113,19 +116,67 @@ def brain_controller(
 
                     # To match how the old brain works (only integer plate angles)
                     pitch, roll = int(pitch), int(roll)
-
                     action = Vector2(-roll, pitch)
-                else:
-                    if alert_fn is not None:
-                        alert_fn(True)
 
             except requests.exceptions.ConnectionError as e:
                 print(f"No brain listening on port: {port}", file=sys.stderr)
                 raise BrainNotFound
+            except Exception as e:
+                print(f"Brain exception: {e}")
+        return action, info
 
+    def next_action_v2(state):
+        env_state, ball_detected, buttons = state
+        x, y, vel_x, vel_y, sum_x, sum_y = env_state
+
+        observables = {
+            "state": {
+                "ball_x": x,
+                "ball_y": y,
+                "ball_vel_x": vel_x,
+                "ball_vel_y": vel_y,
+            }
+        }
+
+        action = Vector2(0, 0)  # Action is 0,0 if not detected or brain didn't work
+        info = {"status": 400, "resp": ""}
+        if ball_detected:
+
+            # Trap on GET failures so we can restart the brain without
+            # bringing down this run loop. Plate will default to level
+            # when it loses the connection.
+            try:
+                # Get action from brain
+                response = requests.post(prediction_url, json=observables)
+                info = {"status": response.status_code, "resp": response.json()}
+                print(response.ok)
+
+                if response.ok:
+                    concepts = info["resp"]["concepts"]
+                    concept_name = list(concepts.keys())[0]  # Just use first concept
+                    pitch = concepts[concept_name]["action"]["input_pitch"]
+                    roll = concepts[concept_name]["action"]["input_roll"]
+
+                    # Scale and clip
+                    pitch = np.clip(pitch * max_angle, -max_angle, max_angle)
+                    roll = np.clip(roll * max_angle, -max_angle, max_angle)
+
+                    # To match how the old brain works (only integer plate angles)
+                    pitch, roll = int(pitch), int(roll)
+                    action = Vector2(-roll, pitch)
+
+            except requests.exceptions.ConnectionError as e:
+                print(f"No brain listening on port: {port}", file=sys.stderr)
+                raise BrainNotFound
             except Exception as e:
                 print(f"Brain exception: {e}")
 
+        print(f"Brain action: {action}")
         return action, info
 
-    return next_action
+    if version == 1:
+        return next_action_v1
+    elif version == 2:
+        return next_action_v2
+    else:
+        raise ValueError("Brain version `{self.version}` is not supported.")
