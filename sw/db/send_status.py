c2e28bc586b48d1d9c3d50dc1788b56adeebd434
Sergei Ziuzev <sergei.z@email.com>
2022-05-13 10:15:18 +0300

    Refactoring send status of ball

--- sw/db/send_status.py
+++ sw/db/send_status.py
@@ -18,62 +18,75 @@ def getHostIP():
     local_ip = s.getsockname()[0]
     return local_ip
 
-class ball_detected:
-    ball_status = None
+class Ball:
+    __moab_local_ip = None
+    __local_ip = None
+    __product = None
+    __status_of_ball = None
+    __ball = None
 
-    IP = getHostIP()
-    PRODUCT_ID = u'ball detect'
-    PRODUCT_NAME = str(IP).replace(".", "")
+    def __init__(self) -> None:
+        self.__local_ip = getHostIP()
+        self.__moab_local_ip = str(self.__local_ip).replace(".", "")
+        self.__product = u'ball detect'
+        self.__status_of_ball = False
+        self.__ball = {
+            u'PartitionKey': self.__moab_local_ip,
+            u'RowKey': self.__product,
+            u'IP': self.__local_ip,
+            u'STATUS': self.__status_of_ball
+        }
 
-    my_entity = {
-        u'PartitionKey': PRODUCT_NAME,
-        u'RowKey': PRODUCT_ID,
-        u'IP': IP,
-        u'STATUS': False
-    }
+    def getEntity(self, status_of_ball):
+        self.__ball["STATUS"] = status_of_ball
+        return self.__ball
 
-    replaced = {
-        u'PartitionKey': PRODUCT_NAME,
-        u'RowKey': PRODUCT_ID,
-        u'IP': IP,
-        u'STATUS': False
-    }
+class Send:
+    __connection_string = None
+    __table_service_client = None
+    __table_name = None
 
-    connection_string = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=moabseziuzmtc;AccountKey=nnQ2NJWQt8JAtU2mJNQ2+9SvpxatO3qvAo+4xIQRCPYycX5S+UlwF6c5awf743UXvKxmcstQZ3kGdFLJs5bIcw=="
+    def __init__(self) -> None:
+        self.__table_name = "BALL"
+        self.__connection_string = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=moabseziuzmtc;AccountKey=nnQ2NJWQt8JAtU2mJNQ2+9SvpxatO3qvAo+4xIQRCPYycX5S+UlwF6c5awf743UXvKxmcstQZ3kGdFLJs5bIcw=="
+        self.__table_service_client = TableServiceClient.from_connection_string(conn_str=self.__connection_string).get_table_client(table_name=self.__table_name)
     
-    my_filter = "PartitionKey eq '{}'".format(PRODUCT_NAME)
-    table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
-    table_client = table_service_client.get_table_client(table_name="BALL")
+    def sendStatusOfBall(self, ball):
+        try:
+            self.__table_service_client.update_entity(mode=UpdateMode.MERGE, entity=ball)
+        except ResourceExistsError as e:
+            print (e)
+    
+    def createRow(self, ball):
+        try:
+            entity = self.__table_service_client.create_entity(entity=ball)
+        except ResourceExistsError as e:
+            print(e)
         
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
 
     conn = None
 
-    connection_string = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=moabseziuzmtc;AccountKey=nnQ2NJWQt8JAtU2mJNQ2+9SvpxatO3qvAo+4xIQRCPYycX5S+UlwF6c5awf743UXvKxmcstQZ3kGdFLJs5bIcw=="
-    table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
-    table_client = table_service_client.get_table_client(table_name="BALL")
+    send = Send()
+    ball = Ball()
 
-    try:
-        entity = ball_detected.table_client.create_entity(entity=ball_detected.my_entity)
-    except ResourceExistsError as e:
-        print(e)
+    status_of_ball = ball.getEntity(False)
+    send.createRow(status_of_ball)
 
-    # finally:
-    #     if conn:
-    #         conn.close()
+    old_status_of_ball = None
 
     while True:
         try:
             statusOfBall = sqlite3.connect(db_file).execute("SELECT status from BALL").fetchone()[0]
-            # cursor = conn.execute("SELECT status from BALL")
-            if statusOfBall == "1":
-                ball_detected.replaced["STATUS"] = True
-            else:
-                ball_detected.replaced["STATUS"] = False
-            ball_detected.table_client.update_entity(mode=UpdateMode.MERGE, entity=ball_detected.replaced)
+            if old_status_of_ball != statusOfBall:
+                if statusOfBall == "1":
+                    status_of_ball = ball.getEntity(True)
+                else:
+                    status_of_ball = ball.getEntity(False)
+                send.sendStatusOfBall(status_of_ball)
+                old_status_of_ball = statusOfBall
 
-            time.sleep(1)
         except Error as e:
             print(e)
 
@@ -81,6 +94,7 @@ def create_connection(db_file):
             if conn:
                 conn.close()
 
+        time.sleep(1)
                 
 if __name__ == '__main__':
     create_connection(r"/home/pi/moab/sw/db/ball.db")
